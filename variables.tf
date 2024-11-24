56c4f7fd82fee651dbf430aabd8033d413f9a6fb
awsadmin <awsadmin@mail.com>
2024-11-24 13:00:58 +0400

    First working release

---
+++ variables.tf
@@ -0,0 +1,40 @@
+variable "create_vpc" {
+  description = "Controls VPC creation"
+  type        = bool
+  default     = true
+}
+
+variable "cidr_block" {
+  description = "VPC CIDR block"
+  type        = string
+}
+
+variable "create_public_subnets" {
+  description = "Controls public subnets creation"
+  type        = bool
+  default     = true
+}
+
+variable "public_subnets" {
+  description = "List of public subnets"
+  type        = list(string)
+  default     = []
+}
+
+variable "create_private_subnets" {
+  description = "Controls private subnets creation"
+  type        = bool
+  default     = true
+}
+
+variable "private_subnets" {
+  description = "List of private subnets"
+  type        = list(string)
+  default     = []
+}
+
+variable "create_nat_gateway" {
+  description = "Controls creation of NAT gateway"
+  type        = bool
+  default     = false
+}
